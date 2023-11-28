73d08cc255c299bb7c126828979b5db062e064f4
Gabriella Caroline PN <lalagabriella03@gmail.com>
2023-11-28 19:25:03 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:pert12/firebase_options.dart';
+import 'package:pert12/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Firestore CRUD',
+      home: HomePage(),
+    );
+  }
+}
